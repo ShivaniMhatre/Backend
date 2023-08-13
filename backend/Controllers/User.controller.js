00c04754f1b3a3ff8d6d36d88c2b8666c52b7b5f
ShivaniMhatre <shivanikatkar97@gmail.com>
2023-08-13 19:47:24 +0530

    updates

--- backend/Controllers/User.controller.js
+++ backend/Controllers/User.controller.js
@@ -1,7 +1,62 @@
+import User from '../Modals/User.modal.js';
+
 export const Login = (req, res) => {
     res.send("Login from Controller")
 }
 
-export const Register = (req, res) => {
-   res.send("Register from Controller")
+export const Register = async (req, res) => {
+    // console.log(req.body, "req.body")
+    const { name, surname, age, email, number, password, confirmPassword } = req.body;
+    if (!name) return res.send("Name is missing..");
+    if (!surname) return res.send("Surname is missing..")
+    if (!age) return res.send("Age is missing..")
+    if (!email) return res.send("Email is required.")
+    if (!number) return res.send("Number is required");
+    if (!password) return res.send("Password is required");
+    if (!confirmPassword) return res.send("Confirm password is required!")
+    if (password !== confirmPassword) return res.send("Password and Confirm password not matched.")
+    const user = new User({
+        name: name,
+        surname: surname,
+        age: parseInt(age),
+        email,
+        number: parseInt(number),
+        password
+    })
+    await user.save()
+    res.send("Registeration Done..")
 }
+
+export const Find = async (req, res) => {
+    const { email } = req.body;
+
+    if (!email) return res.send("Email Is Manditory")
+    const user = await User.find({ email: email })
+    console.log(user, "Users Here");
+    if (user.length) {
+        return res.send(user)
+    }
+    return res.send("No User Found")
+}
+
+export const Update = async (req, res) => {
+    // console.log(req.body, "req.body")
+    const { age, number } = req.body;
+    const { id } = req.params;
+    if (!id) return res.send("Id is required!!!");
+    if (!age) return res.send("Age is reqired!!!");
+    if (!number) return res.send("Number is required");
+
+    const updateuser = await User.findByIdAndUpdate(id, { age, number }, { new: true });
+    //  return res.send("Data Updated....")
+    return res.json({ message: "Data Updated....", data: updateuser })
+}
+
+export const Delete = async (req, res) => {
+    const { id } = req.query;
+    if (!id) return res.send("Id is required!!!")
+
+    const deleteuser = await User.findByIdAndDelete(id);
+    return res.json({ message: "Data Deleted", data: deleteuser })
+}
+
--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,38 @@
 import express from 'express'
-import { Login, Register } from './Controllers/User.controller.js';
+import { Delete, Find, Login, Register, Update } from './Controllers/User.controller.js';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import User from './Modals/User.modal.js';
 
 const app=express();
+dotenv.config();
+app.use(express.json());
+
 app.get("/",function(req,res){
     res.send("working....")
 })
+
 // app.get("/login",function(req,res){
 //     res.send("Login....")
 // })
 app.get("/login",Login)
 
-app.get("/register",Register)
+// CREATE
+app.post("/register",Register)
 // app.get("/register",function(req,res){
 //     res.send("Register....")
 // })
 
-mongoose.connect("mongodb+srv://shivani:shivani123@cluster0.t5nkzfc.mongodb.net/awdiznode").then(()=>{
+// READ
+app.get("/find",Find)
+
+// UPDATE
+
+app.patch("/update/:id",Update)
+
+app.delete("/delete",Delete)
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("Connected to DB")
 }).catch((error)=>{
 console.log("Error While Connecting MongoDB",error)
